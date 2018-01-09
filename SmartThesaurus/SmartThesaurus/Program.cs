e678218ccbd538d77274bd69a9bc5f131595324d
Unknown <cactus318@hotmail.fr>
2018-01-09 16:34:15 +0100

    Tests de lecture d'un fichier texte

--- SmartThesaurus/SmartThesaurus/Program.cs
+++ SmartThesaurus/SmartThesaurus/Program.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SmartThesaurus
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader("hello.txt"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
         }
+
+       
     }
 }
