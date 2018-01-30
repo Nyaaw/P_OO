3c5078751d20d0c8e43473b44493238c97567d97
Guillaume <guillaumedr0@gmail.com>
2018-01-30 13:31:06 +0100

    Change
    Ajout de données dans le visual studio du projet

--- SmartThesaurus/SmartThesaurus/Program.cs
+++ SmartThesaurus/SmartThesaurus/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Word = Microsoft.Office.Interop.Word;
+
 
 namespace SmartThesaurus
 {
@@ -26,8 +28,78 @@ namespace SmartThesaurus
                 Console.WriteLine(e.Message);
             }
             Console.ReadLine();
+
+            ///////////////LIRE UN FICHIER TXT////////////////////////
+
+            //using (StreamReader reader = new StreamReader("../../Test2.docx"))
+            //{
+            //    string[] test;
+
+            //    string content = reader.ReadToEnd();
+            //    reader.Close();
+
+            //    test = content.Split(' ');
+
+            //    foreach(string element in test)
+            //    {
+            //        Console.WriteLine(element);
+            //    }
+
+
+
+
+            //    Console.ReadLine();
+            //}
+
+            ///////////////////////////////////////////////////////////
+
+
+            ///////////////LIRE UN FICHIER DOCX////////////////////////
+
+            object path = @"K:\INF\Eleves\Temp\LOL.docx";
+
+            string txtPath = "TxtOfWord.txt";
+
+            Word.Application app = new Word.Application();
+            Word.Document doc;
+            object missing = Type.Missing;
+            object readOnly = true;
+            try
+            {
+                doc = app.Documents.Open(ref path, ref missing, ref readOnly, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing);
+                string text = doc.Content.Text;
+                //File.WriteAllText(txtPath, text);
+                Console.WriteLine("Converted!");
+            }
+            catch
+            {
+                Console.WriteLine("An error occured. Please check the file path to your word document, and whether the word document is valid.");
+            }
+            finally
+            {
+                object saveChanges = Word.WdSaveOptions.wdDoNotSaveChanges;
+                app.Quit(ref saveChanges, ref missing, ref missing);
+            }
+
+            ///////////////////////////////////////////////////////////
+
+            //////////////////LIRE UN FICHIER XLX/////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //////////////////////////////////////////////////////
         }
 
-       
+
     }
 }
