9f64aaed5112f4114b2996f326d5fc0f843c1e2d
Unknown <cactus318@hotmail.fr>
2018-01-30 13:18:01 +0100

    Avancement

--- SmartThesaurus/SmartThesaurus/Program.cs
+++ SmartThesaurus/SmartThesaurus/Program.cs
@@ -13,7 +13,7 @@ namespace SmartThesaurus
         {
             try
             {   // Open the text file using a stream reader.
-                using (StreamReader sr = new StreamReader("hello.txt"))
+                using (StreamReader sr = new StreamReader("hello.cs"))
                 {
                     // Read the stream to a string, and write the string to the console.
                     String line = sr.ReadToEnd();
