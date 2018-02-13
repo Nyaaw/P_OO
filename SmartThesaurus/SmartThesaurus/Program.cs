3323f6990715a99c82e0acdaaa2a38b1612f724c
Unknown <cactus318@hotmail.fr>
2018-02-13 16:33:54 +0100

    Test de fonctionnement des bouts de code

--- SmartThesaurus/SmartThesaurus/Program.cs
+++ SmartThesaurus/SmartThesaurus/Program.cs
@@ -5,57 +5,26 @@ using System.IO;
 using System.Text;
 using Microsoft.Office.Interop.Word;
 using System.Windows;
-
+using RasterEdge.Imaging.Basic;
+using RasterEdge.XDoc.Converter;
+using RasterEdge.XDoc.PDF;
+using System.Collections.Generic;
+using System.Drawing;
+using Microsoft.Office.Interop.Excel;
+using System.Threading;
 
 namespace SmartThesaurus
 {
     class Program
     {
+
         [STAThread]
         public static void Main()
         {
-
-
-            ///////////////LIRE UN FICHIER TXT////////////////////////
-
-            //try
-            //{   // Open the text file using a stream reader.
-            //    using (StreamReader sr = new StreamReader(@"..\..\TestsLecture\hello.txt"))
-            //    {
-            //        // Read the stream to a string, and write the string to the console.
-            //        String line = sr.ReadToEnd();
-            //        Console.WriteLine(line);
-            //    }
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine("The file could not be read:");
-            //    Console.WriteLine(e.Message);
-            //}
-            //Console.ReadLine();
-
-            ///////////////////////////////////////////////////////////
-
-
-            ///////////////LIRE UN FICHIER DOCX////////////////////////
-
-            object path = @"F:\ETML\P_OO\P_OO\SmartThesaurus\SmartThesaurus\TestsLecture\LOL.docx";
-
-            string txtPath = "TxtOfWord.txt";
-
-            Word.Application app = new Word.Application();
-            Document doc;
-            object missing = Type.Missing;
-            object readOnly = true;
-            try
-
-
-            //////////////////////////////////////////////////////////
-
             /////////////////Lire un document Excel///////////////////
 
-            const string fileName = @"C:\Users\dutoitrugu\Desktop\test.xlsx";
-           
+            const string fileName = @"F:\ETML\P_OO\P_OO\SmartThesaurus\SmartThesaurus\TestsLecture\test.xlsx";
+
             // Ouvrir un ducument Excel
             var application = new Microsoft.Office.Interop.Excel.Application();
             var workbook = application.Workbooks.Open(fileName);
@@ -69,50 +38,48 @@ namespace SmartThesaurus
             //Vas lire tout le texte cellule par cellule jusqu'à ce que la cellule sois null et vas l'ajouter dans une variable séparer d'espace
             do
             {
-                doc = app.Documents.Open(ref path, ref missing, ref readOnly, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing);
-                string text = doc.Content.Text;
-                File.WriteAllText(txtPath, text, new UTF8Encoding());
-                Console.WriteLine("Converted!");
 
-                doc.ActiveWindow.Selection.WholeStory();
-                doc.ActiveWindow.Selection.Copy();
+                worksheet.Cells.FindNext("");
+                //if (worksheet.Cells[i, j].text != null)
+                //    text += worksheet.Cells[i, j].text + " ";
 
-                string ClipboardText;
+                if (i % 100 == 0)
+                    Console.WriteLine(i);
 
-                if (Clipboard.ContainsText(TextDataFormat.Text))
+                j++;
+
+                if (j == 20)
                 {
-                    ClipboardText = Clipboard.GetText(TextDataFormat.Text);
-                }
-                else
-                {
-                    ClipboardText = "Data not found";
+                    j = 1;
+                    i++;
                 }
 
-                Console.WriteLine(ClipboardText);
+            } while (i != 50);
 
-                doc.Close(WdSaveOptions.wdDoNotSaveChanges, WdOriginalFormat.wdOriginalDocumentFormat);
-            }
-            catch
-            {
-                Console.WriteLine("An error occured. Please check the file path to your word document, and whether the word document is valid.");
-            }
-            finally
-            {
-                object saveChanges = WdSaveOptions.wdDoNotSaveChanges;
-                app.Quit(ref saveChanges, ref missing, ref missing);
-            }
+            Console.WriteLine(text);
+
+            //Save.
+            workbook.Save();
+            workbook.Close();
 
             Console.ReadLine();
 
-            //Save.
-            //workbook.Save();
-            ///////////////////////////////////////////////////////////
+            /* UTILISER CE CODE ???
 
-            //////////////////LIRE UN FICHIER XLX/////////////////////
-
-            //////////////////////////////////////////////////////////
+            var usedRange = worksheet.UsedRange;
+            int startRow = usedRange.Row;
+            int endRow = startRow + usedRange.Rows.Count - 1;
+            int startColumn = usedRange.Column;
+            int endColumn = startColumn + usedRange.Columns.Count - 1;
+            for (int row = startRow; row <= endRow; row++)
+            {
+                Excel.Range lastCell = worksheet.Cells[row, endColumn];
+                if (lastCell.Value2 == null)
+                    lastCell = lastCell.End[Excel.XlDirection.xlToLeft];
+                var lastColumn = lastCell.Column;
+                Console.WriteLine($"{row}: {lastColumn}");
+            }
+            */
         }
-
-
     }
 }
